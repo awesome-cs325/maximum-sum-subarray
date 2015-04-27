9fef9baab9effffe1a8e643fd195eb2c70edbedb
Kenneth Hafdahl <hafdahlk@gmail.com>
2015-04-26 20:47:44 -0700

    Added report template and modified graphs.py to include silent option.

--- src/graphs.py
+++ src/graphs.py
@@ -1,25 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sc
+import sys
+import argparse
 
 import timings
 
-def plot(timedict):
+
+def plot(timedict, silentFlag):
     xs = timedict.pop("ns")
     for alg, times in timedict.items():
         ys = ([np.mean(ts) for ts in times])
 
         plt.plot(xs,ys,label=alg)
-
     plt.xlabel('array size')
     plt.ylabel('time')
 
     plt.legend()
     plt.title("Running Times of Various\nMaximum Sum Subarray Algorithms")
 
-    plt.savefig("../bin/testrun.png",bbox_inches="tight")
-    plt.show()
+    plt.savefig("../bin/testrun.eps",bbox_inches="tight")
+    if not silentFlag:
+        plt.show()
 
 
-
-plot(timings.timefuncs())
+if __name__ == "__main__":
+    silentFlag = False
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--silent", action="store_true")
+    args = parser.parse_args()
+    plot(timings.timefuncs(), args.silent)
